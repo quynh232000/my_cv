8076cece6dd8ba284d4ac7c829d3f460ce35d0ef
quynh <quynh232000@gmail.com>
2026-03-31 23:41:35 +0700

    'Build'

--- src/pages/Home/components/index.js
+++ src/pages/Home/components/index.js
@@ -2,4 +2,6 @@ export { default as Education } from "./Education";
 export { default as Work } from "./Work";
 export { default as About } from "./About";
 export { default as Quotes } from "./Quotes";
+
+
 export { default as Testimonial } from "./Testimonial";
